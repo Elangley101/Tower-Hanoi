260efba7b277e6b4d2b485d8f46684d974728a77
Ethan Langley <98287743+Elangley101@users.noreply.github.com>
2023-04-10 18:38:23 -0400

    Add files via upload

---
+++ frmMain.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace TowerHanoi
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartHanoiTower = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTotalDisks = new System.Windows.Forms.TextBox();
+            this.txtMoves = new System.Windows.Forms.TextBox();
+            this.btnCancelSolution = new System.Windows.Forms.Button();
+            this.btnStartPlayingMusic = new System.Windows.Forms.Button();
+            this.btnStopPlayingMusic = new System.Windows.Forms.Button();
+            this.pbCurrentProgress = new System.Windows.Forms.ProgressBar();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartHanoiTower
+            // 
+            this.btnStartHanoiTower.Location = new System.Drawing.Point(28, 413);
+            this.btnStartHanoiTower.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStartHanoiTower.Name = "btnStartHanoiTower";
+            this.btnStartHanoiTower.Size = new System.Drawing.Size(330, 53);
+            this.btnStartHanoiTower.TabIndex = 3;
+            this.btnStartHanoiTower.Text = "Start solving the &Hanoi Tower";
+            this.btnStartHanoiTower.UseVisualStyleBackColor = true;
+            this.btnStartHanoiTower.Click += new System.EventHandler(this.btnStartHanoiTower_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the number of disk:";
+            // 
+            // txtTotalDisks
+            // 
+            this.txtTotalDisks.Location = new System.Drawing.Point(274, 35);
+            this.txtTotalDisks.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTotalDisks.Name = "txtTotalDisks";
+            this.txtTotalDisks.Size = new System.Drawing.Size(148, 26);
+            this.txtTotalDisks.TabIndex = 1;
+            // 
+            // txtMoves
+            // 
+            this.txtMoves.Location = new System.Drawing.Point(28, 75);
+            this.txtMoves.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMoves.Multiline = true;
+            this.txtMoves.Name = "txtMoves";
+            this.txtMoves.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMoves.Size = new System.Drawing.Size(668, 266);
+            this.txtMoves.TabIndex = 2;
+            // 
+            // btnCancelSolution
+            // 
+            this.btnCancelSolution.Location = new System.Drawing.Point(366, 413);
+            this.btnCancelSolution.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCancelSolution.Name = "btnCancelSolution";
+            this.btnCancelSolution.Size = new System.Drawing.Size(330, 53);
+            this.btnCancelSolution.TabIndex = 4;
+            this.btnCancelSolution.Text = "&Cancel the Solution";
+            this.btnCancelSolution.UseVisualStyleBackColor = true;
+            this.btnCancelSolution.Click += new System.EventHandler(this.btnCancelSolution_Click);
+            // 
+            // btnStartPlayingMusic
+            // 
+            this.btnStartPlayingMusic.Location = new System.Drawing.Point(28, 479);
+            this.btnStartPlayingMusic.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStartPlayingMusic.Name = "btnStartPlayingMusic";
+            this.btnStartPlayingMusic.Size = new System.Drawing.Size(330, 53);
+            this.btnStartPlayingMusic.TabIndex = 5;
+            this.btnStartPlayingMusic.Text = "Start &Playing the Music";
+            this.btnStartPlayingMusic.UseVisualStyleBackColor = true;
+            this.btnStartPlayingMusic.Click += new System.EventHandler(this.btnStartPlayingMusic_Click);
+            // 
+            // btnStopPlayingMusic
+            // 
+            this.btnStopPlayingMusic.Location = new System.Drawing.Point(366, 479);
+            this.btnStopPlayingMusic.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStopPlayingMusic.Name = "btnStopPlayingMusic";
+            this.btnStopPlayingMusic.Size = new System.Drawing.Size(330, 53);
+            this.btnStopPlayingMusic.TabIndex = 6;
+            this.btnStopPlayingMusic.Text = "&Stop Playing the Music";
+            this.btnStopPlayingMusic.UseVisualStyleBackColor = true;
+            this.btnStopPlayingMusic.Click += new System.EventHandler(this.btnStopPlayingMusic_Click);
+            // 
+            // pbCurrentProgress
+            // 
+            this.pbCurrentProgress.Location = new System.Drawing.Point(28, 358);
+            this.pbCurrentProgress.Name = "pbCurrentProgress";
+            this.pbCurrentProgress.Size = new System.Drawing.Size(668, 37);
+            this.pbCurrentProgress.TabIndex = 7;
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Location = new System.Drawing.Point(331, 367);
+            this.lblPercent.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(32, 20);
+            this.lblPercent.TabIndex = 0;
+            this.lblPercent.Text = "0%";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 553);
+            this.Controls.Add(this.txtMoves);
+            this.Controls.Add(this.txtTotalDisks);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStopPlayingMusic);
+            this.Controls.Add(this.btnStartPlayingMusic);
+            this.Controls.Add(this.btnCancelSolution);
+            this.Controls.Add(this.btnStartHanoiTower);
+            this.Controls.Add(this.pbCurrentProgress);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tower of Hanoi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartHanoiTower;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTotalDisks;
+        private System.Windows.Forms.TextBox txtMoves;
+        private System.Windows.Forms.Button btnCancelSolution;
+        private System.Windows.Forms.Button btnStartPlayingMusic;
+        private System.Windows.Forms.Button btnStopPlayingMusic;
+        private System.Windows.Forms.ProgressBar pbCurrentProgress;
+        private System.Windows.Forms.Label lblPercent;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TowerHanoi
+{
+    public partial class frmMain : Form
+    {
+        private SoundPlayer sound = new SoundPlayer(@"soundexample.wav");
+        private int steps = 0;
+        private const int SLEEP_TIME = 50;
+        private CancellationTokenSource tokenSource = null;
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+            btnCancelSolution.Enabled = false;
+            btnStopPlayingMusic.Enabled = false;
+
+        }
+        /// <summary>
+        /// Start solving the Hanoi Tower
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void btnStartHanoiTower_Click(object sender, EventArgs e)
+        {
+            int totalDisks;
+            if (!int.TryParse(txtTotalDisks.Text, out totalDisks) || totalDisks < 1)
+            {
+                MessageBox.Show("Wrong number of disk");
+                txtTotalDisks.Text = "";
+                txtTotalDisks.Focus();
+                return;
+            }
+            steps = (int)(Math.Pow(2, totalDisks) - 1);
+            btnCancelSolution.Enabled = true;
+            tokenSource = new CancellationTokenSource();
+            CancellationToken ct = tokenSource.Token;
+
+            var progress = new Progress<int>(percent =>
+            {
+                pbCurrentProgress.Value = percent;
+                lblPercent.Text = $"{percent}%";
+            });
+            txtMoves.Text = "";
+            var progresssolveTowers = new Progress<string>(result =>
+            {
+                txtMoves.Text += result+Environment.NewLine;
+            });
+
+            try
+            {
+                bool isCompleted = false;
+                var doProgressTask = Task.Run(() =>
+                {
+                    isCompleted = doProgress(progress, ct, steps);
+                });
+
+                char startPeg = 'A'; // start tower in output
+                char endPeg = 'C'; // end tower in output
+                char tempPeg = 'B'; // temporary tower in output
+                var solveTowersTask = Task.Run(() =>
+                {
+                    isCompleted = solveTowers(progresssolveTowers, ct, totalDisks, startPeg, endPeg, tempPeg);
+                });
+
+                await Task.WhenAll(doProgressTask,solveTowersTask);
+
+                if (isCompleted)
+                {
+                    lblPercent.Text = "Completed";
+                }
+                else
+                {
+                    lblPercent.Text = "Cancelled";
+                }
+                btnCancelSolution.Enabled = false;
+            }
+            catch (OperationCanceledException ex)
+            {
+                lblPercent.Text = "Cancelled";
+                btnCancelSolution.Enabled = false;
+            }
+            finally
+            {
+                tokenSource.Dispose();
+            }
+        }
+        /// <summary>
+        /// do rogress
+        /// </summary>
+        /// <param name="progress"></param>
+        /// <param name="token"></param>
+        /// <param name="steps"></param>
+        /// <returns></returns>
+        private bool doProgress(IProgress<int> progress, CancellationToken token, int steps)
+        {
+            if (token.IsCancellationRequested)
+            {
+                return false;
+            }
+            for (int i = 0; i <= steps; ++i)
+            {
+                Thread.Sleep(SLEEP_TIME);
+                var percent = (i * 100) / steps;
+                if (progress != null)
+                {
+                    progress.Report(percent);
+                }
+                if (token.IsCancellationRequested)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// solve Towers
+        /// </summary>
+        /// <param name="progress"></param>
+        /// <param name="token"></param>
+        /// <param name="n"></param>
+        /// <param name="startPeg"></param>
+        /// <param name="endPeg"></param>
+        /// <param name="tempPeg"></param>
+        /// <returns></returns>
+        private bool solveTowers(IProgress<string> progress, CancellationToken token, int n, char startPeg, char endPeg, char tempPeg)
+        {
+            if (token.IsCancellationRequested)
+            {
+                return false;
+            }
+            if (n > 0)
+            {
+                solveTowers(progress, token, n - 1, startPeg, tempPeg, endPeg);
+                Thread.Sleep(SLEEP_TIME);
+                if (progress != null)
+                {
+                    progress.Report($"Move disk from {startPeg} to {endPeg}");
+                }
+                if (token.IsCancellationRequested)
+                {
+                    return false;
+                }
+                solveTowers(progress, token,n - 1, tempPeg, endPeg, startPeg);
+            }
+            return true;
+            
+        }
+
+        /// <summary>
+        /// Cancel the solution
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancelSolution_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tokenSource.Cancel();
+                tokenSource.Dispose();
+                lblPercent.Text = "Cancelled";
+                btnCancelSolution.Enabled = false;
+            }
+            catch (OperationCanceledException ex)
+            {
+                lblPercent.Text = "Cancelled";
+                btnCancelSolution.Enabled = false;
+                tokenSource.Dispose();
+            }
+
+        }
+        /// <summary>
+        /// Start Playing the music
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStartPlayingMusic_Click(object sender, EventArgs e)
+        {
+            sound.Play();
+            btnStopPlayingMusic.Enabled = true;
+        }
+        /// <summary>
+        /// Stop Playing Music
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStopPlayingMusic_Click(object sender, EventArgs e)
+        {
+            sound.Stop();
+            btnStopPlayingMusic.Enabled = false;
+        }
+    }
+}
